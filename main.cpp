4bdb0e524936034c795b2a3d019f3c8271a789bc
Rodrigo Almeida <82787131+II2DII@users.noreply.github.com>
2021-12-16 16:52:21 -0300

    new

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-
+//1. Todas as classes devem ter no mínimo três construtores, e um deles deve ser o de cópia
+//2. Duas variáveis static
+//4. Todos os métodos que não alteram a classe devem ser const
+//5. No mínimo 4 classes/fiz 2
+//6. Usar o destructor para desalocar memória e se for o caso alterar o valor das variáveis static
+//7. Usar string
+//11. Todas as classes devem ter o seguintes operadores sobrecarregados: <<, >>, =, == e !=
+//13. Demonstrar o uso de todos os requisitos no main usando um menu de opções
 using namespace std;
 
 class Catalogo{
